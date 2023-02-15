6d49d5211cc0dcb804c2ff0b6089af0e26b40173
Gao Lu <glu@tigerconnect.com>
2023-02-15 11:14:38 +0800

    commit

---
+++ lib/lugaoxx1.ex
@@ -0,0 +1,18 @@
+defmodule Lugaoxx1 do
+  @moduledoc """
+  Documentation for `Lugaoxx1`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Lugaoxx1.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/lugaoxx1_test.exs
@@ -0,0 +1,8 @@
+defmodule Lugaoxx1Test do
+  use ExUnit.Case
+  doctest Lugaoxx1
+
+  test "greets the world" do
+    assert Lugaoxx1.hello() == :world
+  end
+end
